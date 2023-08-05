a15cb0a80aa3df3d2233887edc7fd9d1640178f8
Muhammed Kaplan <muhammedkaplann0@gmail.com>
2023-08-06 00:39:15 +0200

    feat: note page

--- apps/frontend/app/dashboard/notes/[slug]/page.tsx
+++ apps/frontend/app/dashboard/notes/[slug]/page.tsx
@@ -1,6 +1,20 @@
-import { FiEdit, FiShare } from "react-icons/fi";
-import ReactMarkdown from "react-markdown";
+"use client";
+import "@uiw/react-markdown-preview/markdown.css";
+import MDEditor from "@uiw/react-md-editor";
+import "@uiw/react-md-editor/markdown-editor.css";
+import { useCallback, useState } from "react";
+import { FiCheck, FiEdit, FiShare } from "react-icons/fi";
+
 export default function Page({ params }: { params: { slug: string } }) {
+  const [editMode, setEditMode] = useState<boolean>(false);
+  const [markdown, setMarkdown] = useState<string>(
+    "# Selam\nnaber\n> iyidir senden\nSome *emphasis* and <strong>strong</strong>!"
+  );
+
+  const toggleEditMote = useCallback(() => {
+    setEditMode(!editMode);
+  }, [editMode]);
+
   return (
     <>
       <div id="header" className="flex justify-between">
@@ -9,19 +23,37 @@ export default function Page({ params }: { params: { slug: string } }) {
         </div>
 
         <div id="actions" className="flex ">
-          <button className="btn text mr-3">
+          <button className="btn text mr-3" onClick={toggleEditMote}>
             <FiEdit />
             Edit project
           </button>
-
-          <button className="btn text">
+          <button className="btn text ">
             <FiShare /> Share project
           </button>
+          {editMode && (
+            <button className="btn primary ml-3" onClick={toggleEditMote}>
+              <FiCheck /> Save
+            </button>
+          )}
         </div>
       </div>
 
-      <div id="content">
-        <ReactMarkdown>{md}</ReactMarkdown>
+      <div
+        id="content"
+        className="border rounded-md bg-gray-100 p-5 mt-10"
+        onDoubleClick={toggleEditMote}
+      >
+        {editMode ? (
+          <MDEditor
+            value={markdown}
+            onChange={(e) => setMarkdown(e!)}
+            preview="edit"
+          />
+        ) : (
+          <div>
+            <MDEditor.Markdown source={markdown} />
+          </div>
+        )}
       </div>
     </>
   );
