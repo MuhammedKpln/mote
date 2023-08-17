bced35efa0a694174dbe6a59d3392a8202475933
Muhammed Kaplan <muhammedkaplann0@gmail.com>
2023-08-17 11:44:22 +0200

    fix: remove persist

--- apps/frontend/app/providers.tsx
+++ apps/frontend/app/providers.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/system";
-import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
-import { QueryClient } from "@tanstack/react-query";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+  dehydrate,
+} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { SessionProvider } from "next-auth/react";
 import * as React from "react";
 import { Toaster } from "react-hot-toast";
@@ -18,26 +21,21 @@ export const queryClient = new QueryClient({
   },
 });
 
-const persister = createSyncStoragePersister({
-  storage: window.localStorage,
-});
-
 export interface ProvidersProps {
   children: React.ReactNode;
 }
 
 export function Providers({ children }: ProvidersProps) {
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <NextUIProvider>
       <SessionProvider>
-        <PersistQueryClientProvider
-          persistOptions={{ persister }}
-          client={queryClient}
-        >
-          {children}
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={dehydratedState}>{children}</Hydrate>
           <ReactQueryDevtools initialIsOpen={false} />
           <Toaster />
-        </PersistQueryClientProvider>
+        </QueryClientProvider>
       </SessionProvider>
     </NextUIProvider>
   );
