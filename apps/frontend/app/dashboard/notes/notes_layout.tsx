c04ea156c451771643c074fd35e1b0ef126cda1d
Muhammed Kaplan <muhammedkaplann0@gmail.com>
2023-08-19 00:15:52 +0200

    revert: tags

--- apps/frontend/app/dashboard/notes/notes_layout.tsx
+++ apps/frontend/app/dashboard/notes/notes_layout.tsx
@@ -1,12 +1,8 @@
-"use client";
 import { MoteSpinner } from "@/components/spinner";
 import styles from "@/styles/dashboard.module.scss";
-import { Tab, Tabs } from "@nextui-org/tabs";
 import { Suspense, useState } from "react";
-import { FiBook, FiTag } from "react-icons/fi";
 import { CreateNote } from "./_components/_create_note";
 import { Notes } from "./_components/_notes";
-import { TagsSidebar } from "./_components/_tags_sidebar";
 import { NotesSidebar } from "./_components/notes_sidebar";
 
 enum ActiveTab {
@@ -22,43 +18,8 @@ export function NotesLayout({ children }: { children: React.ReactNode }) {
       <div id="sidebar" className={styles.notesSidebar}>
         <Suspense fallback={<MoteSpinner />}>
           <NotesSidebar>
-            <Tabs
-              selectedKey={activeTab}
-              onSelectionChange={(e) => setActiveTab(e as ActiveTab)}
-              aria-label="Options"
-              color="primary"
-              variant="bordered"
-              classNames={{
-                base: "w-full",
-                tabList: "w-full m-5",
-              }}
-            >
-              <Tab
-                key={ActiveTab.Notes}
-                title={
-                  <div className="flex items-center space-x-2">
-                    <FiBook />
-                    <span>Notes</span>
-                  </div>
-                }
-              >
-                <Notes />
-                <CreateNote />
-              </Tab>
-              <Tab
-                key={ActiveTab.Tags}
-                title={
-                  <div className="flex items-center space-x-2">
-                    <FiTag />
-                    <span>Tags</span>
-                  </div>
-                }
-              >
-                <TagsSidebar
-                  navigateBackToNotes={() => setActiveTab(ActiveTab.Notes)}
-                />
-              </Tab>
-            </Tabs>
+            <Notes />
+            <CreateNote />
           </NotesSidebar>
         </Suspense>
       </div>
